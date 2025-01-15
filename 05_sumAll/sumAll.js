cd77c03bd9fffcb69df3ecce3ed3231a759636b7
Jamie Atyeo <jamie.atyeo@hootsuite.com>
2025-01-14 17:36:53 -0800

    05_sumAll implement sum, handle < 0, handle non-integers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (typeof(num1) !== Number || typeof(num2) !== Number) {
+        return 'ERROR';
+    }
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
 
+    let start = num1 < num2 ? num1 : num2;
+    let end = num2 >= num1 ? num2 : num1;
+    for (i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
