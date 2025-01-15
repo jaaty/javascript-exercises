01127c2474a31d8f9f7331661940b5144e7bc685
Jamie Atyeo <jamie.atyeo@hootsuite.com>
2025-01-14 23:14:15 -0800

    06_leapYears readjust logic to properly handle centuries

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
 const leapYears = function(year) {
-    if (year % 100 !== 0 && (year % 4 === 0 || year % 400 === 0)) {
-        return true;
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
     } else {
         return false;
     }
