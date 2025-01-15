b7d5d8892dfa72442bdb4622f9b22c53b64cbbcb
Jamie Atyeo <jamie.atyeo@hootsuite.com>
2025-01-14 17:02:35 -0800

    02_repeatString handle negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(word, numberOfTimes) {
     let repeatedWord = '';
+    if (numberOfTimes < 0) {
+        return 'ERROR';
+    }
     for (let i = 0; i < numberOfTimes; i++) {
         repeatedWord += word;
     }
